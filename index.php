b3935b2f85faf2a6abb5237e801a2e935f9e251e
Larry Mayers <larry.mayers@protexxa-barbados.com>
2023-09-27 23:24:20 -0400

    Base design review for updated Athlene Academics LMS

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Athlene Academics</title>
+            </head>
+    <body>
+        <header></header>
+        <section>
+            <div id="screen">
+                <h1>Athlene Academics | Customized Learning For You!</h1>
+                    <button><a href="tel:12462319428">Enrol Now!</a></button>
+            </div>
+        </section>
+        <footer></footer>
+    </body>
+</html>
