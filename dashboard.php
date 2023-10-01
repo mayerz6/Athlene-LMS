dfcbd206150f4d92cadc53b9c47d51cdef5c3109
Larry Mayers <larry.mayers@protexxa-barbados.com>
2023-10-01 15:37:23 -0400

    Simple design of Athlene's student management dashboard UI

---
+++ dashboard.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head><title></title></head>
+        <body>
+            <div id="mainPan">
+                <div id="leftPan">
+                    <header></header>
+
+                </div>
+            </div>
+         
+            <div id="screen">
+                <h1>Athlene Academics - Student Management Dashboard</h1>
+
+                <small>Please review your students progress below.</small>
+            </div>
+         
+            <footer></footer>
+        </body>
+</html>
