8de9790b848f3a404fa730f0f50a6b53fa7116de
Larry Mayers <larry.mayers@protexxa-barbados.com>
2023-10-09 13:48:49 -0400

    Simple UI design of site's contact form

---
+++ contact.php
@@ -0,0 +1,12 @@
+
+<?php require_once("./regions/header.php"); ?>
+
+<section>
+<div id="screen">
+    <h1>Athlene Academics - Let's Stay In Touch</h1>
+        <small>Please select your preferred means of communication.</small>
+            <h4>+1(246)4292662</h4>
+</div>
+</section>
+
+<?php require_once("./regions/footer.php"); ?>
