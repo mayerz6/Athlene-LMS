05b5372b72775d56bffc6c13e4c6dd359a47c3f5
Larry Mayers <larry.mayers@protexxa-barbados.com>
2023-10-12 14:46:30 -0400

    Configuration of the database CLASS meant to faciliate DB communications with the web app

--- core/db.php
+++ core/db.php
@@ -1,14 +1,23 @@
 <?php
 require __DIR__ . "/config.php";
 
-$dsn = "mysql:host={$config['database']['hostname']};dbname={$config['database']['database']}";
 
-try { $pdo = new PDO($dsn, $config['database']['username'], $config['database']['password']); echo "Successful Connection!"; }
-catch(PDOException $e) { echo $e->getMessage(); }
 
-class db {
-/* Instantiation of the DATABASE Class used to structure
-processes & procedures associated within database
-CRUD operations. */
+class DBConnect {
+    /* Instantiation of the DATABASE Class used to structure
+    processes & procedures associated within database
+    CRUD operations. */
+    
+    public static function getInstance($config){
+        $dsn = "mysql:host={$config['database']['hostname']};dbname={$config['database']['database']};charset=utf8";
+        try { $pdo = new PDO($dsn, $config['database']['username'], $config['database']['password']); echo "Successful Connection!"; }
+        catch(PDOException $e) { echo $e->getMessage(); }
+
+        return $pdo;
+    }
 
 }
+
+$db1 = new DBConnect;
+
+$db1->getInstance($config);
--- login.php
+++ login.php
@@ -1,14 +1,24 @@
 <?php require_once("./regions/header.php"); ?>
+<?php require_once("./core/db.php"); ?>
 
 <?php 
+
+    echo $_SERVER['SCRIPT_NAME'];
+
     if(isset($_POST['submit'])):
+        var_dump($_POST);
+        echo "<br />";
         echo $_SERVER['PHP_SELF'];
+        echo "<br />";
+        echo $_POST['username'];
+        echo "<br />";
+        echo $_POST['password'];
     endif;
 ?>
 
 <section>
-<div id="screen">
-    <h1>Athlene Academics - Account Login Portal</h1>
+    <div id="screen">
+        <h1>Athlene Academics - Account Login Portal</h1>
             <h4>Secure Authorized Access ONLY</h4>
             <form method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
                 <fieldset>
@@ -19,13 +29,14 @@
                    </div>                   
                    <div class="form-group">
                    <label for="password">Enter your secure password:</label>    
-                   <input type="password" name="password" />
+                   <input type="password" name="password" placeholder="Account Password" />
                    </div>
                     <hr />
                    <input type="submit" name="submit" />
+                   <!-- <input type="" name="reset" /> -->
                 </fieldset>
             </form>
-</div>
+    </div>
 </section>
 
 <?php require_once("./regions/footer.php"); ?>
